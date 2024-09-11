db850d151d9219cc74300ebaddd1dbc072b5b1fa
Luke Swanson <lwswanson2@wisc.edu>
2024-09-10 23:46:51 -0500

    Set up TestDriver file.

---
+++ tests/TestDriver.py
@@ -0,0 +1,38 @@
+# Standard imports
+from unittest import defaultTestLoader, TestLoader, TestSuite, runner
+
+# Set up path
+import os, sys
+from pprint import pprint
+from pathlib import Path
+
+sys.path.insert(0, str(Path(os.getcwd()) / "src"))
+# Set up logging
+import logging
+from ogd.common.utils.Logger import Logger
+Logger.InitializeLogger(level=logging.INFO, use_logfile=False)
+from ogd.common.schemas.configs.TestConfigSchema import TestConfigSchema
+
+from config.t_config import settings
+
+_config = TestConfigSchema.FromDict(name="APIUtilsTestConfig", all_elements=settings, logger=None)
+
+# loader = TestLoader()
+# TODO : At the moment, this is just module-level, should eventually go to class-level selection.
+suite = TestSuite()
+if _config.EnabledTests.get('INTERFACES'):
+    print("***\nAdding t_interfaces:")
+    suite.addTest(defaultTestLoader.discover('./tests/cases/t_interfaces/', pattern="t_*.py", top_level_dir="./"))
+    print("Done\n***")
+if _config.EnabledTests.get('UTILS'):
+    print("***\nAdding t_APIUtils:")
+    suite.addTest(defaultTestLoader.discover('./tests/cases/t_utils/', pattern="t_*.py", top_level_dir="./"))
+    print("Done\n***")
+
+print(f"Tests are:")
+for ts in suite._tests:
+    if isinstance(ts, TestSuite):
+        pprint([t._tests if isinstance(t, TestSuite) else t for t in ts])
+
+testRunner = runner.TextTestRunner()
+testRunner.run(suite)
--- tests/config/t_config.py
+++ tests/config/t_config.py
@@ -1,10 +1,8 @@
 settings = {
     "VERBOSE" : True,
     "ENABLED" : {
-        "RESPONSE":False,
-        "UTILS":False,
-        "CONFIG":False,
-        "HELLO":True
+        "INTERFACES":True,
+        "UTILS":True
     },
     "REMOTE_ADDRESS" : "127.0.0.1:5000"
 }
