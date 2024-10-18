9c1332a77a21d8740b05d2737156c1618f5b9cda
Luke Swanson <lwswanson2@wisc.edu>
2024-10-17 22:25:53 -0400

    Default the logger to None.

--- src/ogd/common/schemas/Schema.py
+++ src/ogd/common/schemas/Schema.py
@@ -52,7 +52,7 @@ class Schema(abc.ABC):
     # *** PUBLIC STATICS ***
 
     @classmethod
-    def ElementFromDict(cls, all_elements:Dict[str, Any], element_names:List[str], parser_function:Callable, default_value:Any, logger:Optional[logging.Logger]) -> Any:
+    def ElementFromDict(cls, all_elements:Dict[str, Any], element_names:List[str], parser_function:Callable, default_value:Any, logger:Optional[logging.Logger]=None) -> Any:
         for name in element_names:
             if name in all_elements:
                 return parser_function(all_elements[name])
