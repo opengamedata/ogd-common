22482d22762a871ddad9e5e80736937665a96c03
Luke Swanson <lwswanson2@wisc.edu>
2024-10-23 17:14:16 -0500

    Correct name for config schema.

--- tests/cases/schemas/t_Schema.py
+++ tests/cases/schemas/t_Schema.py
@@ -11,7 +11,7 @@ from ogd.common.utils.Logger import Logger
 from src.ogd.common.schemas.Schema import Schema
 from tests.config.t_config import settings
 
-_config = TestConfigSchema.FromDict(name="ServerConfigSchemaTestConfig", all_elements=settings, logger=None)
+_config = TestConfigSchema.FromDict(name="SchemaTestConfig", all_elements=settings, logger=None)
 
 @unittest.skip("No tests implemented yet")
 class t_Schema(TestCase):
