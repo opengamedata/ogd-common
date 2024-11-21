d88cc3f596472dce4d09f8278cc057bf9e2325a7
Luke Swanson <lwswanson2@wisc.edu>
2024-11-20 23:35:00 -0600

    If SetFilter is given an empty list/set/whatever, warn that this is not the same as a NoFilter.

--- src/ogd/common/connectors/filters/SetFilter.py
+++ src/ogd/common/connectors/filters/SetFilter.py
@@ -1,8 +1,14 @@
+## import standard libraries
+import logging
 from typing import Any
+# import local files
+from ogd.common.utils.Logger import Logger
 from ogd.common.connectors.filters.Filter import Filter
 
 class SetFilter(Filter):
     def __init__(self, set_elements:Any):
+        if len(set_elements) == 0:
+            Logger.Log("Creating a SetFilter from an empty set! This will be intepreted as filtering out all values, not as an empty filter that allows all values to pass! For the latter case, use the NoFilter class.", logging.WARNING)
         self._set = set(set_elements)
 
     @property
