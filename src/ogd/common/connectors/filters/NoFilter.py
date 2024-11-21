c4ad0eeee2078544253695e7bcf2900a8c3e6b3e
Luke Swanson <lwswanson2@wisc.edu>
2024-11-21 10:00:44 -0600

    Make use of NOFILTER mode as default for Filter, and automatic for NoFilter class.

--- src/ogd/common/connectors/filters/NoFilter.py
+++ src/ogd/common/connectors/filters/NoFilter.py
@@ -2,5 +2,5 @@ from ogd.common.connectors.filters.Filter import Filter
 from ogd.common.models.enums.FilterMode import FilterMode
 
 class NoFilter(Filter):
-    def __init__(self, mode:FilterMode):
-        super().__init__(mode=mode)
+    def __init__(self):
+        super().__init__(mode=FilterMode.NOFILTER)
