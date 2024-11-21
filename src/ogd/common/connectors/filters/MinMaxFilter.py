c774609092b0644f8bba237e7ccc0ab279c82424
Luke Swanson <lwswanson2@wisc.edu>
2024-11-20 23:29:34 -0600

    Warn if we accidentally gave a min larger than max.

--- src/ogd/common/connectors/filters/MinMaxFilter.py
+++ src/ogd/common/connectors/filters/MinMaxFilter.py
@@ -1,8 +1,14 @@
+## import standard libraries
+import logging
 from typing import Any
+# import local files
+from ogd.common.utils.Logger import Logger
 from ogd.common.connectors.filters.Filter import Filter
 
 class MinMaxFilter(Filter):
     def __init__(self, minimum:Any, maximum:Any):
+        if minimum > maximum:
+            Logger.Log(f"When creating MinMaxFilter, got a minimum ({minimum}) larger than maximum ({maximum})!", level=logging.WARNING)
         self._min = minimum
         self._max = maximum
 
