2ccb4a623560aca455a5e75227a358eed6a31954
Luke Swanson <lwswanson2@wisc.edu>
2024-11-25 15:10:32 -0600

    Add an init file for the filter collections.

---
+++ src/ogd/common/connectors/filters/collections/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    "EventFilterCollection",
+    "IDFilterCollection",
+    "TimingFilterCollection",
+    "VersioningFilterCollection"
+]
+
+from .EventFilterCollection import EventFilterCollection
+from .IDFilterCollection import IDFilterCollection
+from .TimingFilterCollection import TimingFilterCollection
+from .VersioningFilterCollection import VersioningFilterCollection
