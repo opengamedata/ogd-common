b7f99b7e56af34580659077101c5be30a644b474
Luke Swanson <lwswanson2@wisc.edu>
2024-11-29 23:08:15 -0600

    Fix for name of parameter to GameSourceSchema class.

--- tests/cases/interfaces/t_CSVInterface.py
+++ tests/cases/interfaces/t_CSVInterface.py
@@ -64,7 +64,7 @@ class t_CSVInterface(TestCase):
 
     def test_DatesFromIDs(self):
         with self.zipped_file.open(self.zipped_file.namelist()[0]) as f:
-            _cfg = GameSourceSchema(name="FILE SOURCE", all_elements={"SCHEMA":"OGD_EVENT_FILE", "DB_TYPE":"FILE"}, data_sources={})
+            _cfg = GameSourceSchema(name="FILE SOURCE", other_elements={"SCHEMA":"OGD_EVENT_FILE", "DB_TYPE":"FILE"}, data_sources={})
             CSVI = CSVInterface(game_id='BACTERIA', config=_cfg, filepath=f, delim='\t', fail_fast=False)
             if CSVI.Open():
                 dates = CSVI.DatesFromIDs(self.TEST_SESSION_LIST)
