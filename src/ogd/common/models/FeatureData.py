d68be73dff272fe2f43bb48dfb346ff3c8826a0e
Luke Swanson <lwswanson2@wisc.edu>
2024-12-01 00:45:40 -0600

    Make FeatureData inherit from GameData as well.

--- src/ogd/common/models/FeatureData.py
+++ src/ogd/common/models/FeatureData.py
@@ -1,54 +1,48 @@
 from typing import Any, List, Optional
 
+from ogd.common.models.GameData import GameData
 from ogd.common.models.enums.ExtractionMode import ExtractionMode
 
-class FeatureData:
-   def __init__(self, name:str, feature_type:str, count_index:Optional[int],
-                cols:List[str], vals:List[Any],   mode:ExtractionMode,
-                player_id:Optional[str]=None,     sess_id:Optional[str]=None):
-      self._name = name
-      self._feature_type = feature_type
-      self._count_index = count_index
-      self._cols = cols
-      self._vals = vals
-      self._mode = mode
-      self._player_id = player_id
-      self._sess_id = sess_id
+class FeatureData(GameData):
+    def __init__(self, name:str,            feature_type:str,         count_index:Optional[int],
+                 cols:List[str],            vals:List[Any],           mode:ExtractionMode,
+                 app_id:str,                user_id:Optional[str],    session_id:str,
+                 app_version:Optional[str], app_branch:Optional[str], log_version:Optional[str]):
+        super().__init__(app_id=app_id,           user_id=user_id,       session_id=session_id,
+                         app_version=app_version, app_branch=app_branch, log_version=log_version)
+        self._name = name
+        self._feature_type = feature_type
+        self._count_index = count_index
+        self._cols = cols
+        self._vals = vals
+        self._mode = mode
 
-   def __str__(self):
-      return f"Name: {self.Name}\tCount Index: {self.CountIndex}\nColumns: {self._cols}\t Values: {self._vals}\nMode: {self._mode.name}\tPlayer: {self.PlayerID}\tSession: {self.SessionID}"
+    def __str__(self) -> str:
+        return f"Name: {self.Name}\tCount Index: {self.CountIndex}\nColumns: {self._cols}\t Values: {self._vals}\nMode: {self._mode.name}\tPlayer: {self.PlayerID}\tSession: {self.SessionID}"
 
-   def __repr__(self):
-      return self.Name
+    def __repr__(self) -> str:
+        return self.Name
 
-   @property
-   def Name(self):
-      return self._name
+    @property
+    def Name(self):
+        return self._name
 
-   @property
-   def FeatureType(self):
-      return self._feature_type
+    @property
+    def FeatureType(self):
+        return self._feature_type
 
-   @property
-   def CountIndex(self):
-      return self._count_index
+    @property
+    def CountIndex(self):
+        return self._count_index
 
-   @property
-   def FeatureNames(self) -> List[str]:
-      return self._cols
+    @property
+    def FeatureNames(self) -> List[str]:
+        return self._cols
 
-   @property
-   def FeatureValues(self) -> List[Any]:
-      return self._vals
+    @property
+    def FeatureValues(self) -> List[Any]:
+        return self._vals
 
-   @property
-   def ExportMode(self):
-      return self._mode
-
-   @property
-   def PlayerID(self) -> Optional[str]:
-      return self._player_id
-
-   @property
-   def SessionID(self) -> Optional[str]:
-      return self._sess_id
+    @property
+    def ExportMode(self):
+        return self._mode
