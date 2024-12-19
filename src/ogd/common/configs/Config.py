533e2967382fc0c8012fca0931d6e589263b9933
Luke Swanson <lwswanson2@wisc.edu>
2024-12-18 20:36:04 -0600

    On second thought, don't even make Config a subclass of Schema, make it a straight-up type alias instead.

--- src/ogd/common/configs/Config.py
+++ src/ogd/common/configs/Config.py
@@ -1,13 +1,8 @@
 """Config Class Module
 """
 ## import standard libraries
-from typing import Any, Dict
+from typing import TypeAlias
 # import local files
 from ogd.common.schemas.Schema import Schema
 
-class Config(Schema):
-    """Thin layer over Schema base class to act as a base for all our Config-type classes.
-    """
-    
-    def __init__(self, name: str, other_elements: Dict[str, Any] | None = None):
-        super().__init__(name, other_elements)
+Config : TypeAlias = Schema
