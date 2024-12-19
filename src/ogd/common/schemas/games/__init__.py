ece4b65e4098c7c82a251749cf8f1c2364fd82bc
Luke Swanson <lwswanson2@wisc.edu>
2024-12-18 19:35:29 -0600

    Remove non-existant old classes from schemas/games/__init__.py.

--- src/ogd/common/schemas/games/__init__.py
+++ src/ogd/common/schemas/games/__init__.py
@@ -1,17 +1,7 @@
 __all__ = [
     "EventSchema",
-    "GeneratorConfig",
-    "DetectorConfig",
-    "FeatureConfig",
-    "AggregateConfig",
-    "PerCountConfig",
     "GameSchema"
 ]
 
 from . import EventSchema
-from . import GeneratorConfig
-from . import DetectorConfig
-from . import FeatureConfig
-from . import AggregateConfig
-from . import PerCountConfig
 from . import GameSchema
