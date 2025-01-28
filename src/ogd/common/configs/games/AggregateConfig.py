0d99133d29f4e0c552ca3b8b81033d0daef4fd64
Luke Swanson <lwswanson2@wisc.edu>
2025-01-27 19:50:14 -0600

    Fix other linter warnings from those two classes.

--- src/ogd/common/configs/games/AggregateConfig.py
+++ src/ogd/common/configs/games/AggregateConfig.py
@@ -1,11 +1,14 @@
+"""Aggregate Feature Config Module
+"""
 # import standard libraries
-import logging
 from typing import Any, Dict, Optional
 # import local files
 from ogd.common.configs.games.FeatureConfig import FeatureConfig
 from ogd.common.utils.typing import Map
 
 class AggregateConfig(FeatureConfig):
+    """Schema for tracking the configuration of an aggregate feature.
+    """
     def __init__(self, name:str, other_elements:Optional[Map]=None):
         super().__init__(name=name, other_elements=other_elements)
 
--- src/ogd/common/configs/games/DetectorConfig.py
+++ src/ogd/common/configs/games/DetectorConfig.py
@@ -1,5 +1,6 @@
+"""Detector Config Module
+"""
 # import standard libraries
-import logging
 from typing import Any, Dict, Optional
 # import local files
 from ogd.common.models.enums.ExtractionMode import ExtractionMode
@@ -7,6 +8,8 @@ from ogd.common.configs.games.GeneratorConfig import GeneratorConfig
 from ogd.common.utils.typing import Map
 
 class DetectorConfig(GeneratorConfig):
+    """Schema for tracking the configuration of a detector.
+    """
     def __init__(self, name:str, other_elements:Optional[Map]=None):
         super().__init__(name=name, other_elements=other_elements)
 
