1f3b5ad5d41909fefaab452b955a3012e50d47be
Luke Swanson <lwswanson2@wisc.edu>
2025-04-25 11:57:44 -0500

    Update typing module tests to use To<Type> instead of _parse<Type>.

--- tests/cases/utils/test_typing.py
+++ tests/cases/utils/test_typing.py
@@ -60,128 +60,128 @@ class ToInt(TestCase):
         _int = conversions.ToInt(name="ParseIntVal", value=_nan)
         self.assertIsNone(_int)
 
-class parseFloat(TestCase):
+class ToFloat(TestCase):
     def test_normal_float(self):
-        _float = conversions._parseFloat(name="ParseFloatVal", value=1.2)
+        _float = conversions.ToFloat(name="ParseFloatVal", value=1.2)
         self.assertIsInstance(_float, float)
         self.assertEqual(_float, 1.2)
 
     def test_int_to_float(self):
-        _float = conversions._parseFloat(name="ParseFloatVal", value=1)
+        _float = conversions.ToFloat(name="ParseFloatVal", value=1)
         self.assertIsInstance(_float, float)
         self.assertEqual(_float, 1.)
 
     def test_wrongtype(self):
         _nan = {1:2}
-        _float = conversions._parseFloat(name="ParseFloatVal", value=_nan)
+        _float = conversions.ToFloat(name="ParseFloatVal", value=_nan)
         self.assertIsNone(_float)
 
-class parseString(TestCase):
+class ToString(TestCase):
     def test_normal_string(self):
-        _str = conversions._parseString(name="ParseStringVal", value="Foo")
+        _str = conversions.ToString(name="ParseStringVal", value="Foo")
         self.assertIsInstance(_str, str)
         self.assertEqual(_str, "Foo")
 
     def test_int_to_str(self):
-        _str = conversions._parseString(name="ParseStringVal", value=123)
+        _str = conversions.ToString(name="ParseStringVal", value=123)
         self.assertIsInstance(_str, str)
         self.assertEqual(_str, "123")
 
     def test_dict_to_str(self):
         _elem = {1:2}
         _elem_str = "{1: 2}"
-        _str = conversions._parseString(name="ParseStringVal", value=_elem)
+        _str = conversions.ToString(name="ParseStringVal", value=_elem)
         self.assertIsInstance(_str, str)
         self.assertEqual(_str, _elem_str)
 
-class parsePath(TestCase):
+class ToPath(TestCase):
     def test_normal_path(self):
         _val = Path("./Foo")
-        _path = conversions._parsePath(name="ParsePathVal", value=_val)
+        _path = conversions.ToPath(name="ParsePathVal", value=_val)
         self.assertIsInstance(_path, Path)
         self.assertEqual(_path, _val)
 
     def test_pathstring(self):
         _elem = Path("./Foo")
         _elem_str = "./Foo"
-        _path = conversions._parsePath(name="ParsePathVal", value=_elem_str)
+        _path = conversions.ToPath(name="ParsePathVal", value=_elem_str)
         self.assertIsInstance(_path, Path)
         self.assertEqual(_path, _elem)
 
     def test_wrongtype(self):
-        _not_path = conversions._parsePath(name="ParseStringVal", value=123)
+        _not_path = conversions.ToPath(name="ParseStringVal", value=123)
         self.assertIsNone(_not_path)
 
 @unittest.skip(reason="Not implemented")
-class parseDatetime(TestCase):
+class ToDatetime(TestCase):
     def test_normal_timezone(self):
         _val = datetime.datetime(2020, 1, 1)
-        _str = conversions._parseDatetime(name="parseDatetimeVal", value=_val)
+        _str = conversions.ToDatetime(name="ToDatetimeVal", value=_val)
         self.assertIsInstance(_str, str)
         self.assertEqual(_str, "Foo")
 
 @unittest.skip(reason="Not implemented")
-class parseTimedelta(TestCase):
+class ToTimedelta(TestCase):
     def test_normal_timezone(self):
         _val = datetime.timedelta(hours=1)
-        _str = conversions._parseTimedelta(name="parseTimedeltaVal", value=_val)
+        _str = conversions.ToTimedelta(name="ToTimedeltaVal", value=_val)
         self.assertIsInstance(_str, str)
         self.assertEqual(_str, "Foo")
 
 @unittest.skip(reason="Not implemented")
-class parseTimezone(TestCase):
+class ToTimezone(TestCase):
     def test_normal_timezone(self):
         _val = datetime.timezone(datetime.timedelta(hours=1))
-        _str = conversions._parseTimezone(name="parseTimezoneVal", value=_val)
+        _str = conversions.ToTimezone(name="ToTimezoneVal", value=_val)
         self.assertIsInstance(_str, str)
         self.assertEqual(_str, "Foo")
 
-class parseList(TestCase):
+class ToList(TestCase):
     def test_normal_list(self):
         _elems = [1, 2.2, "3"]
-        _list = conversions._parseList(name="parseListVal", value=_elems)
+        _list = conversions.ToList(name="ToListVal", value=_elems)
         self.assertIsInstance(_list, list)
         self.assertEqual(_list, _elems)
 
     def test_dictstring(self):
         _elems = [1, 2.2, "3"]
         _elems_str = '[1, 2.2, "3"]'
-        _list = conversions._parseList(name="parseListVal", value=_elems_str)
+        _list = conversions.ToList(name="ToListVal", value=_elems_str)
         self.assertIsInstance(_list, list)
         self.assertEqual(_list, _elems)
 
     def test_nullstring(self):
-        with self.subTest(msg="_parseList: string = 'None'"):
+        with self.subTest(msg="ToList: string = 'None'"):
             _str = "None"
-            _list = conversions._parseList(name="parseListVal", value=_str)
+            _list = conversions.ToList(name="ToListVal", value=_str)
             self.assertIsNone(_list)
-        with self.subTest(msg="_parseList: string = 'null'"):
+        with self.subTest(msg="ToList: string = 'null'"):
             _str = "null"
-            _list = conversions._parseList(name="parseListVal", value=_str)
+            _list = conversions.ToList(name="ToListVal", value=_str)
             self.assertIsNone(_list)
-        with self.subTest(msg="_parseList: string = ''"):
+        with self.subTest(msg="ToList: string = ''"):
             _str = ""
-            _list = conversions._parseList(name="parseListVal", value=_str)
+            _list = conversions.ToList(name="ToListVal", value=_str)
             self.assertIsNone(_list)
 
     def test_nonlist(self):
         _val = 42
-        _list = conversions._parseList(name="parseListVal", value=_val)
+        _list = conversions.ToList(name="ToListVal", value=_val)
         self.assertIsNone(_list)
 
     def test_bad_liststring(self):
         _elems = "['foo', bar]"
-        _list = conversions._parseList(name="parseListVal", value=_elems)
+        _list = conversions.ToList(name="ToListVal", value=_elems)
         self.assertIsNone(_list)
 
-class parseJSON(TestCase):
+class ToJSON(TestCase):
     def test_normal_dict(self):
         _elems = {
             "foo":1,
             "bar":2.2,
             "baz":"3"
         }
-        _dict = conversions._parseJSON(name="parseJSONVal", value=_elems)
+        _dict = conversions.ToJSON(name="ToJSONVal", value=_elems)
         self.assertIsInstance(_dict, dict)
         self.assertEqual(_dict, _elems)
 
@@ -196,32 +196,32 @@ class parseJSON(TestCase):
             "bar":2.2,
             "baz":"3"
         }'''
-        _dict = conversions._parseJSON(name="parseJSONVal", value=_elems_str)
+        _dict = conversions.ToJSON(name="ToJSONVal", value=_elems_str)
         self.assertIsInstance(_dict, dict)
         self.assertEqual(_dict, _elems)
 
     def test_nullstring(self):
-        with self.subTest(msg="_parseJSON: string = 'None'"):
+        with self.subTest(msg="ToJSON: string = 'None'"):
             _str = "None"
-            _dict = conversions._parseJSON(name="parseJSONVal", value=_str)
+            _dict = conversions.ToJSON(name="ToJSONVal", value=_str)
             self.assertIsNone(_dict)
-        with self.subTest(msg="_parseJSON: string = 'null'"):
+        with self.subTest(msg="ToJSON: string = 'null'"):
             _str = "null"
-            _dict = conversions._parseJSON(name="parseJSONVal", value=_str)
+            _dict = conversions.ToJSON(name="ToJSONVal", value=_str)
             self.assertIsNone(_dict)
-        with self.subTest(msg="_parseJSON: string = ''"):
+        with self.subTest(msg="ToJSON: string = ''"):
             _str = ""
-            _dict = conversions._parseJSON(name="parseJSONVal", value=_str)
+            _dict = conversions.ToJSON(name="ToJSONVal", value=_str)
             self.assertIsNone(_dict)
 
     def test_nondict(self):
         _val = 42
-        _dict = conversions._parseJSON(name="parseJSONVal", value=_val)
+        _dict = conversions.ToJSON(name="ToJSONVal", value=_val)
         self.assertIsNone(_dict)
 
     def test_bad_dictstring(self):
         _elems = "{ 'foo':bar }"
-        _dict = conversions._parseJSON(name="parseJSONVal", value=_elems)
+        _dict = conversions.ToJSON(name="ToJSONVal", value=_elems)
         self.assertIsNone(_dict)
 
 if __name__ == '__main__':
