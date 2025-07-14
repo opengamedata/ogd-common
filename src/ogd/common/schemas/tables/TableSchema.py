8a9a97d8af26dda48e6a2e0df6e12ac04822339b
Luke Swanson <lwswanson2@wisc.edu>
2025-07-13 23:24:02 -0500

    Update TableSchema functioning after new work on location schemas.
    In particular, make it look for a specifier of the type of location in
    whatever config is being read, and load the specific type of location
    schema subclass.
    In theory, could use some sort of factory approach for this in the
    future.
    
    Also, update the _parseStructure to allow config to specify a file with
    the actual structure schema config, rather than requiring the dict we
    set up from to contain the whole spec directly.

--- src/ogd/common/schemas/tables/TableSchema.py
+++ src/ogd/common/schemas/tables/TableSchema.py
@@ -1,5 +1,7 @@
 ## import standard libraries
 import logging
+import json
+from pathlib import Path
 from typing import Any, Dict, List, Optional, TypeAlias
 ## import local files
 from ogd.common.schemas.Schema import Schema
@@ -7,7 +9,10 @@ from ogd.common.schemas.tables.ColumnSchema import ColumnSchema
 from ogd.common.schemas.tables.TableStructureSchema import TableStructureSchema
 from ogd.common.schemas.tables.EventTableStructureSchema import EventTableStructureSchema
 from ogd.common.schemas.tables.FeatureTableStructureSchema import FeatureTableStructureSchema
-from ogd.common.schemas.locations.TableLocationSchema import URLLocationSchema
+from ogd.common.schemas.locations.LocationSchema import LocationSchema
+from ogd.common.schemas.locations.FileLocationSchema import FileLocationSchema
+from ogd.common.schemas.locations.URLLocationSchema import URLLocationSchema
+from ogd.common.schemas.locations.DatabaseLocationSchema import DatabaseLocationSchema
 from ogd.common.utils.Logger import Logger
 from ogd.common.utils.typing import Map
 
@@ -23,9 +28,10 @@ class TableSchema(Schema):
 
     _DEFAULT_TABLE_TYPE = "EVENT"
     _DEFAULT_STRUCTURE = {}
+    _DEFAULT_LOCATION_TYPE = "FILE"
     _DEFAULT_LOCATION = {}
 
-    def __init__(self, name, structure:TableStructureSchema, location:URLLocationSchema, other_elements:Optional[Map]):
+    def __init__(self, name, structure:TableStructureSchema, location:LocationSchema, other_elements:Optional[Map]):
         """Constructor for the TableStructureSchema class.
         Given a database connection and a game data request,
         this retrieves a bit of information from the database to fill in the
@@ -42,7 +48,8 @@ class TableSchema(Schema):
         unparsed_elements : Map = other_elements or {}
         _table_type = self._parseTableType(unparsed_elements=unparsed_elements)
         self._structure : TableStructureSchema = structure or self._parseStructure(name=f"{name}Structure", table_type=_table_type, unparsed_elements=unparsed_elements)
-        self._location  : URLLocationSchema  = location  or self._parseLocation(name=f"{name}Location", unparsed_elements=unparsed_elements)
+        _location_type = self._parseLocationType(unparsed_elements=unparsed_elements)
+        self._location  : LocationSchema  = location  or self._parseLocation(name=f"{name}Location", location_type=_location_type, unparsed_elements=unparsed_elements)
 
         # after loading the file, take the stuff we need and store.
         super().__init__(name=name, other_elements=other_elements)
@@ -61,7 +68,7 @@ class TableSchema(Schema):
         return self._structure.Columns
 
     @property
-    def Location(self) -> URLLocationSchema:
+    def Location(self) -> LocationSchema:
         return self._location
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
@@ -90,9 +97,10 @@ class TableSchema(Schema):
         """
         _table_type_str : str                  = cls._parseTableType(unparsed_elements=unparsed_elements)
         _structure      : TableStructureSchema = cls._parseStructure(name=name, table_type=_table_type_str, unparsed_elements=unparsed_elements)
-        _location       : URLLocationSchema  = cls._parseLocation(name=name, unparsed_elements=unparsed_elements)
+        _loc_type_str   : str                  = cls._parseLocationType(unparsed_elements=unparsed_elements)
+        _location       : LocationSchema       = cls._parseLocation(name=name, location_type=_loc_type_str, unparsed_elements=unparsed_elements)
 
-        _used = {"table_type", "structure", "location"}
+        _used = {"table_type", "structure", "location_type", "location"}
         _leftovers = { key : val for key,val in unparsed_elements.items() if key not in _used }
         return TableSchema(name=name, structure=_structure, location=_location, other_elements=_leftovers)
 
@@ -101,7 +109,7 @@ class TableSchema(Schema):
         return TableSchema(
             name="DefaultTableSchema",
             structure=EventTableStructureSchema.Default(),
-            location=URLLocationSchema.Default(),
+            location=FileLocationSchema.Default(),
             other_elements={}
         )
 
@@ -112,46 +120,68 @@ class TableSchema(Schema):
     # *** PRIVATE STATICS ***
 
     @staticmethod
-    def _parseTableType(unparsed_elements:Map) -> str:
+    def _parseTableType(unparsed_elements:Map, keys:List[str]=["table_type"]) -> str:
         return TableSchema.ParseElement(
             unparsed_elements=unparsed_elements,
-            valid_keys=["table_type"],
+            valid_keys=keys,
             to_type=str,
             default_value=TableSchema._DEFAULT_TABLE_TYPE,
             remove_target=True
         )
 
     @staticmethod
-    def _parseStructure(name:str, table_type:str, unparsed_elements:Map) -> TableStructureSchema:
+    def _parseStructure(name:str, table_type:str, unparsed_elements:Map, keys:List[str]=["structure"]) -> TableStructureSchema:
         ret_val : TableStructureSchema
 
         _structure = TableSchema.ParseElement(
             unparsed_elements=unparsed_elements,
-            valid_keys=["structure"],
-            to_type=dict,
+            valid_keys=keys,
+            to_type=[Path, dict], # check first if we got a string that parses to a path for the file, otherwise assume whole structure is given.
             default_value=TableSchema._DEFAULT_STRUCTURE,
             remove_target=True
         )
         match table_type.upper():
             case "EVENT":
-                ret_val = EventTableStructureSchema.FromDict(name=f"{name}EventStructure", unparsed_elements=_structure)
+                ret_val =  EventTableStructureSchema.FromFile(schema_name=_structure.name, schema_path=_structure.parent) \
+                        if type(_structure) == Path else \
+                           EventTableStructureSchema.FromDict(name=f"{name}EventStructure", unparsed_elements=_structure)
+                          
             case "FEATURE":
-                ret_val = FeatureTableStructureSchema.FromDict(name=f"{name}EventStructure", unparsed_elements=_structure)
+                ret_val =  FeatureTableStructureSchema.FromFile(schema_name=_structure.name, schema_path=_structure.parent) \
+                        if type(_structure) == Path else \
+                           FeatureTableStructureSchema.FromDict(name=f"{name}EventStructure", unparsed_elements=_structure)
+                
         
         return ret_val
 
     @staticmethod
-    def _parseLocation(name:str, unparsed_elements:Map) -> URLLocationSchema:
-        ret_val : URLLocationSchema
+    def _parseLocationType(unparsed_elements:Map, keys:List[str]=["location_type"]) -> str:
+        return TableSchema.ParseElement(
+            unparsed_elements=unparsed_elements,
+            valid_keys=keys,
+            to_type=str,
+            default_value=TableSchema._DEFAULT_LOCATION_TYPE,
+            remove_target=True
+        )
+
+    @staticmethod
+    def _parseLocation(name:str, location_type:str, unparsed_elements:Map, keys:List[str]=["location"]) -> LocationSchema:
+        ret_val : LocationSchema
 
         _location = TableSchema.ParseElement(
             unparsed_elements=unparsed_elements,
-            valid_keys=["location"],
+            valid_keys=keys,
             to_type=dict,
             default_value=TableSchema._DEFAULT_LOCATION,
             remove_target=True
         )
-        ret_val = URLLocationSchema.FromDict(name=f"{name}Location", unparsed_elements=_location)
+        match location_type.upper():
+            case "FILE":
+                ret_val = FileLocationSchema.FromDict(name=f"{name}Location", unparsed_elements=_location)
+            case "URL":
+                ret_val = URLLocationSchema.FromDict(name=f"{name}Location", unparsed_elements=_location)
+            case "DB" | "DATABASE":
+                ret_val = DatabaseLocationSchema.FromDict(name=f"{name}Location", unparsed_elements=_location)
 
         return ret_val
 
