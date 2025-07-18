6357df73cf433070f5425dbec2d32e69554c6eeb
Luke Swanson <lwswanson2@wisc.edu>
2025-07-18 18:39:54 -0500

    Rename GameDatasetCollectionSchema -> DatasetCollectionSchema.

--- src/ogd/common/schemas/datasets/DatasetCollectionSchema.py
+++ src/ogd/common/schemas/datasets/DatasetCollectionSchema.py
@@ -11,7 +11,7 @@ from ogd.common.schemas.datasets.DatasetSchema import DatasetSchema
 from ogd.common.utils.typing import Map
 
 # Simple class to manage a mapping of dataset names to dataset schemas.
-class GameDatasetCollectionSchema(Schema):
+class DatasetCollectionSchema(Schema):
     """Simple class to manage a mapping of dataset names to dataset schemas.
 
     This exists separately from `DatasetCollectionSchema` because there is,
@@ -25,7 +25,7 @@ class GameDatasetCollectionSchema(Schema):
     def __init__(self, name:str, game_datasets:Dict[str, DatasetSchema], other_elements:Dict[str, Any]):
         unparsed_elements : Map = other_elements or {}
 
-        self._game_datasets : Dict[str, DatasetSchema] = game_datasets or self._parseGameDatasets(unparsed_elements=unparsed_elements)
+        self._game_datasets : Dict[str, DatasetSchema] = game_datasets or self._parseDatasets(unparsed_elements=unparsed_elements)
 
         super().__init__(name=name, other_elements={})
 
@@ -44,7 +44,7 @@ class GameDatasetCollectionSchema(Schema):
         return ret_val
 
     @classmethod
-    def _fromDict(cls, name:str, unparsed_elements:Map, key_overrides:Optional[Dict[str, str]]=None)-> "GameDatasetCollectionSchema":
+    def _fromDict(cls, name:str, unparsed_elements:Map, key_overrides:Optional[Dict[str, str]]=None)-> "DatasetCollectionSchema":
         """_summary_
 
         TODO : Add example of what format unparsed_elements is expected to have.
@@ -54,18 +54,18 @@ class GameDatasetCollectionSchema(Schema):
         :param unparsed_elements: _description_
         :type unparsed_elements: Dict[str, Any]
         :return: _description_
-        :rtype: GameDatasetCollectionSchema
+        :rtype: DatasetCollectionSchema
         """
-        _game_datasets : Dict[str, DatasetSchema] = cls._parseGameDatasets(unparsed_elements=unparsed_elements)
+        _game_datasets : Dict[str, DatasetSchema] = cls._parseDatasets(unparsed_elements=unparsed_elements)
 
-        return GameDatasetCollectionSchema(name=name, game_datasets=_game_datasets, other_elements={})
+        return DatasetCollectionSchema(name=name, game_datasets=_game_datasets, other_elements={})
 
     # *** PUBLIC STATICS ***
 
     @classmethod
-    def Default(cls) -> "GameDatasetCollectionSchema":
-        return GameDatasetCollectionSchema(
-            name="DefaultGameDatasetCollectionSchema",
+    def Default(cls) -> "DatasetCollectionSchema":
+        return DatasetCollectionSchema(
+            name="DefaultDatasetCollectionSchema",
             game_datasets=cls._DEFAULT_DATASETS,
             other_elements={}
         )
@@ -75,7 +75,7 @@ class GameDatasetCollectionSchema(Schema):
     # *** PRIVATE STATICS ***
 
     @staticmethod
-    def _parseGameDatasets(unparsed_elements:Map) -> Dict[str, DatasetSchema]:
+    def _parseDatasets(unparsed_elements:Map) -> Dict[str, DatasetSchema]:
         ret_val : Dict[str, DatasetSchema]
 
         ret_val = {
