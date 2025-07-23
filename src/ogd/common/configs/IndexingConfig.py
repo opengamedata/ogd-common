12a69b9f3d3b0f42a9b001d816ba7f552b7a79bd
Luke Swanson <lwswanson2@wisc.edu>
2025-07-22 21:49:14 -0500

    Set up IndexingConfig to use location schemas.

--- src/ogd/common/configs/IndexingConfig.py
+++ src/ogd/common/configs/IndexingConfig.py
@@ -1,18 +1,23 @@
 # import standard libraries
 from pathlib import Path
 from typing import Dict, Optional
+from urllib.parse import urlparse, ParseResult
 # import local files
 from ogd.common.configs.Config import Config
+from ogd.common.schemas.locations.DirectoryLocationSchema import DirectoryLocationSchema
+from ogd.common.schemas.locations.URLLocationSchema import URLLocationSchema
 from ogd.common.utils.typing import Map
 
 class FileIndexingConfig(Config):
-    _DEFAULT_LOCAL_DIR  = Path("./data/")
-    _DEFAULT_REMOTE_URL = "https://opengamedata.fielddaylab.wisc.edu/opengamedata/"
-    _DEFAULT_TEMPLATE_URL  = "https://github.com/opengamedata/opengamedata-samples"
+    _DEFAULT_LOCAL_DIR  = DirectoryLocationSchema(name="DefaultLocalDir", folder_path=Path("./data/"), other_elements={})
+    _DEFAULT_REMOTE_RAW = ParseResult(scheme="https", netloc="opengamedata.fielddaylab.wisc.edu", path="opengamedata", params="", query="", fragment="")
+    _DEFAULT_REMOTE_URL = URLLocationSchema(name="DefaultRemoteURL", url=_DEFAULT_REMOTE_RAW)
+    _DEFAULT_TEMPLATE_RAW = ParseResult(scheme="https", netloc="github.com", path="opengamedata/opengamedata-samples", params="", query="", fragment="")
+    _DEFAULT_TEMPLATE_URL = URLLocationSchema(name="DefaultTemplateURL", url=_DEFAULT_TEMPLATE_RAW)
 
     # *** BUILT-INS & PROPERTIES ***
 
-    def __init__(self, name:str, local_dir:Optional[Path], remote_url:Optional[str], templates_url:Optional[str], other_elements:Optional[Map]=None):
+    def __init__(self, name:str, local_dir:Optional[DirectoryLocationSchema], remote_url:Optional[URLLocationSchema], templates_url:Optional[URLLocationSchema], other_elements:Optional[Map]=None):
         """Constructor for the `IndexingConfig` class.
         
         If optional params are not given, data is searched for in `other_elements`.
@@ -40,21 +45,21 @@ class FileIndexingConfig(Config):
         """
         unparsed_elements : Map = other_elements or {}
 
-        self._local_dir     : Path          = local_dir     or self._parseLocalDir(unparsed_elements=unparsed_elements)
-        self._remote_url    : Optional[str] = remote_url    or self._parseRemoteURL(unparsed_elements=unparsed_elements)
-        self._templates_url : str           = templates_url or self._parseTemplatesURL(unparsed_elements=unparsed_elements)
+        self._local_dir     : DirectoryLocationSchema = local_dir     or self._parseLocalDir(unparsed_elements=unparsed_elements)
+        self._remote_url    : URLLocationSchema       = remote_url    or self._parseRemoteURL(unparsed_elements=unparsed_elements)
+        self._templates_url : URLLocationSchema       = templates_url or self._parseTemplatesURL(unparsed_elements=unparsed_elements)
         super().__init__(name=name, other_elements=other_elements)
 
     @property
-    def LocalDirectory(self) -> Path:
+    def LocalDirectory(self) -> DirectoryLocationSchema:
         return self._local_dir
 
     @property
-    def RemoteURL(self) -> Optional[str]:
+    def RemoteURL(self) -> URLLocationSchema:
         return self._remote_url
 
     @property
-    def TemplatesURL(self) -> str:
+    def TemplatesURL(self) -> URLLocationSchema:
         return self._templates_url
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
@@ -106,33 +111,59 @@ class FileIndexingConfig(Config):
     # *** PRIVATE STATICS ***
 
     @staticmethod
-    def _parseLocalDir(unparsed_elements:Map) -> Path:
-        return FileIndexingConfig.ParseElement(
+    def _parseLocalDir(unparsed_elements:Map) -> DirectoryLocationSchema:
+        ret_val : DirectoryLocationSchema
+
+        raw_local = FileIndexingConfig.ParseElement(
             unparsed_elements=unparsed_elements,
             valid_keys=["LOCAL_DIR"],
             to_type=Path,
-            default_value=FileIndexingConfig._DEFAULT_LOCAL_DIR,
+            default_value=None,
             remove_target=True
         )
+        # In order for us to control what the default path is, we'll try to parse here and just pass in to constructor, rather than use their FromDict.
+        if raw_local:
+            ret_val = DirectoryLocationSchema(name="RepositoryFilesBase", folder_path=Path(raw_local))
+        else:
+            ret_val = FileIndexingConfig._DEFAULT_LOCAL_DIR
+        
+        return ret_val
 
     @staticmethod
-    def _parseRemoteURL(unparsed_elements:Map) -> str:
-        return FileIndexingConfig.ParseElement(
+    def _parseRemoteURL(unparsed_elements:Map) -> URLLocationSchema:
+        ret_val : URLLocationSchema
+
+        raw_url = FileIndexingConfig.ParseElement(
             unparsed_elements=unparsed_elements,
             valid_keys=["REMOTE_URL"],
             to_type=str,
-            default_value=FileIndexingConfig._DEFAULT_REMOTE_URL,
+            default_value=None,
             remove_target=True
         )
+        if raw_url:
+            as_url = urlparse(raw_url)
+            ret_val = URLLocationSchema(name="RemoteURL", url=as_url)
+        else:
+            ret_val = FileIndexingConfig._DEFAULT_REMOTE_URL
+        return ret_val
 
     @staticmethod
-    def _parseTemplatesURL(unparsed_elements:Map) -> str:
-        return FileIndexingConfig.ParseElement(
+    def _parseTemplatesURL(unparsed_elements:Map) -> URLLocationSchema:
+        ret_val : URLLocationSchema
+
+        raw_url = FileIndexingConfig.ParseElement(
             unparsed_elements=unparsed_elements,
             valid_keys=["TEMPLATES_URL"],
             to_type=str,
-            default_value=FileIndexingConfig._DEFAULT_TEMPLATE_URL,
+            default_value=None,
             remove_target=True
         )
+        if raw_url:
+            as_url = urlparse(raw_url)
+            ret_val = URLLocationSchema(name="TemplatesURL", url=as_url)
+        else:
+            ret_val = FileIndexingConfig._DEFAULT_TEMPLATE_URL
+
+        return ret_val
 
     # *** PRIVATE METHODS ***
