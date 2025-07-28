23192a1f896d3773afe5542522d5dedc3e08c987
Luke Swanson <lwswanson2@wisc.edu>
2025-07-28 17:40:51 -0500

    More work on the mapping schemas.
    In particular, move all the stuff about versioning to EventMapSchema,
    since Features no longer are going to have those specific version bits
    in them.

--- src/ogd/common/schemas/tables/ColumnMapSchema.py
+++ src/ogd/common/schemas/tables/ColumnMapSchema.py
@@ -13,18 +13,12 @@ ColumnMapElement : TypeAlias = Optional[str | List[str] | Dict[str,str]]
 ## @class TableSchema
 class ColumnMapSchema(Schema):
 
-    @classmethod
-    @abc.abstractmethod
-    def _subparseDict(cls, name:str, raw_map:Dict[str, ColumnMapElement], column_schemas:List[ColumnSchema], logger:Optional[logging.Logger]=None) -> Self:
-        pass
-
     # *** BUILT-INS & PROPERTIES ***
 
     _DEFAULT_COLUMNS = []
 
     def __init__(self, name,
                  app_id:Optional[ColumnMapElement],    user_id:Optional[ColumnMapElement],     session_id:Optional[ColumnMapElement],
-                 app_version:Optional[ColumnMapElement], app_branch:Optional[ColumnMapElement], log_version:Optional[ColumnMapElement],
                  other_elements:Optional[Map]=None
         ):
         """Constructor for the TableSchema class.
@@ -38,20 +32,10 @@ class ColumnMapSchema(Schema):
 
         ```
         {
-            "session_id"           : "session_id",
             "app_id"               : null,
-            "timestamp"            : "client_time",
-            "event_name"           : "event_name",
-            "event_data"           : "event_data",
-            "event_source"         : "event_source",
-            "app_version"          : "app_version",
-            "app_branch"           : "app_branch",
-            "log_version"          : "log_version",
-            "time_offset"          : "client_offset",
-            "user_id"              : "user_id",
+            "session_id"           : "session_id",
             "user_data"            : "user_data",
-            "game_state"           : "game_state",
-            "event_sequence_index" : "event_sequence_index"
+            ...
         }
         ```
 
@@ -68,9 +52,6 @@ class ColumnMapSchema(Schema):
         self._app_id      : ColumnMapElement = app_id      or self._parseAppID(unparsed_elements=self._raw_map)
         self._user_id     : ColumnMapElement = user_id     or self._parseUserID(unparsed_elements=self._raw_map)
         self._session_id  : ColumnMapElement = session_id  or self._parseSessionID(unparsed_elements=self._raw_map)
-        self._app_version : ColumnMapElement = app_version or self._parseAppVersion(unparsed_elements=self._raw_map)
-        self._app_branch  : ColumnMapElement = app_branch  or self._parseAppBranch(unparsed_elements=self._raw_map)
-        self._log_version : ColumnMapElement = log_version or self._parseLogVersion(unparsed_elements=self._raw_map)
 
         # after loading the file, take the stuff we need and store.
         super().__init__(name=name, other_elements=other_elements)
@@ -98,18 +79,6 @@ class ColumnMapSchema(Schema):
     @property
     def SessionIDColumn(self) -> ColumnMapElement:
         return self._session_id
-
-    @property
-    def AppVersionColumn(self) -> ColumnMapElement:
-        return self._app_version
-
-    @property
-    def AppBranchColumn(self) -> ColumnMapElement:
-        return self._app_branch
-
-    @property
-    def LogVersionColumn(self) -> ColumnMapElement:
-        return self._log_version
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
 
@@ -140,33 +109,6 @@ class ColumnMapSchema(Schema):
 
     # *** PRIVATE STATICS ***
     
-    # @staticmethod
-    # def _retrieveElement(elem:Any, name:str) -> Optional[ColumnMapElement]:
-    #     """_summary_
-
-    #     :param elem: _description_
-    #     :type elem: Any
-    #     :param name: _description_
-    #     :type name: str
-    #     :return: _description_
-    #     :rtype: Optional[ColumnMapElement]
-    #     """
-    #     ret_val : Optional[str | List[str] | Dict[str, str]]
-    #     if elem is not None:
-    #         if isinstance(elem, str):
-    #             ret_val = elem
-    #         elif isinstance(elem, list):
-    #             ret_val = elem
-    #         elif isinstance(elem, dict):
-    #             ret_val = elem
-    #         else:
-    #             ret_val = str(elem)
-    #             Logger.Log(f"Column name(s) mapped to {name} was not a string or list, defaulting to str(name) == {ret_val} being mapped to {name}", logging.WARN)
-    #     else:
-    #         ret_val = None
-    #         Logger.Log(f"Column name mapped to {name} was left null, nothing will be mapped to {name}", logging.WARN)
-    #     return ret_val
-
     @staticmethod
     def _parseAppID(unparsed_elements:Map) -> Optional[ColumnMapElement]:
         return ColumnMapSchema.ParseElement(
@@ -192,35 +134,6 @@ class ColumnMapSchema(Schema):
         return ColumnMapSchema.ParseElement(
             unparsed_elements=unparsed_elements,
             valid_keys=["session_id"],
-            to_type=[str, list, dict],
-            default_value=None,
-            remove_target=False
-        )
-    @staticmethod
-    def _parseAppVersion(unparsed_elements:Map) -> Optional[ColumnMapElement]:
-        return ColumnMapSchema.ParseElement(
-            unparsed_elements=unparsed_elements,
-            valid_keys=["app_version"],
-            to_type=[str, list, dict],
-            default_value=None,
-            remove_target=False
-        )
-
-    @staticmethod
-    def _parseAppBranch(unparsed_elements:Map) -> Optional[ColumnMapElement]:
-        return ColumnMapSchema.ParseElement(
-            unparsed_elements=unparsed_elements,
-            valid_keys=["app_branch", "app_flavor"],
-            to_type=[str, list, dict],
-            default_value=None,
-            remove_target=False
-        )
-
-    @staticmethod
-    def _parseLogVersion(unparsed_elements:Map) -> Optional[ColumnMapElement]:
-        return ColumnMapSchema.ParseElement(
-            unparsed_elements=unparsed_elements,
-            valid_keys=["log_version"],
             to_type=[str, list, dict],
             default_value=None,
             remove_target=False
--- src/ogd/common/schemas/tables/EventMapSchema.py
+++ src/ogd/common/schemas/tables/EventMapSchema.py
@@ -1,14 +1,10 @@
 """EventTableSchema Module"""
 # import standard libraries
-import logging
-from datetime import datetime, timedelta, timezone
-from typing import Any, Dict, List, Tuple, Optional
+from typing import Dict, Optional, Self
 
 # import local files
 from ogd.common.schemas.tables.TableSchema import ColumnMapElement, ColumnMapElement
 from ogd.common.schemas.tables.ColumnMapSchema import ColumnMapSchema
-from ogd.common.schemas.tables.ColumnSchema import ColumnSchema
-from ogd.common.utils.Logger import Logger
 from ogd.common.utils.typing import Map
 
 ## @class TableSchema
@@ -30,19 +26,68 @@ class EventMapSchema(ColumnMapSchema):
                  game_state:Optional[ColumnMapElement],  user_data:Optional[ColumnMapElement],
                  other_elements:Optional[Map]=None):
         """Constructor for the TableSchema class.
-        Given a database connection and a game data request,
-        this retrieves a bit of information from the database to fill in the
-        class variables.
+        
+        If optional params are not given, data is searched for in `other_elements`.
 
-        :param schema_name: The filename for the table schema JSON.
-        :type schema_name: str
-        :param schema_path: Path to find the given table schema file, defaults to "./schemas/table_schemas/"
-        :type schema_path: str, optional
-        :param is_legacy: [description], defaults to False
-        :type is_legacy: bool, optional
+        The structure is assumed to be as follows:
+
+        ```python
+        {
+            "session_id"           : "session_id",
+            "app_id"               : null,
+            "timestamp"            : "client_time",
+            "event_name"           : "event_name",
+            "event_data"           : "event_data",
+            "event_source"         : "event_source",
+            "app_version"          : "app_version",
+            "app_branch"           : "app_branch",
+            "log_version"          : "log_version",
+            "time_offset"          : "client_offset",
+            "user_id"              : "user_id",
+            "user_data"            : "user_data",
+            "game_state"           : "game_state",
+            "event_sequence_index" : "event_sequence_index"
+        }
+        ```
+
+        :param name: _description_
+        :type name: _type_
+        :param app_id: _description_
+        :type app_id: Optional[ColumnMapElement]
+        :param user_id: _description_
+        :type user_id: Optional[ColumnMapElement]
+        :param session_id: _description_
+        :type session_id: Optional[ColumnMapElement]
+        :param app_version: _description_
+        :type app_version: Optional[ColumnMapElement]
+        :param app_branch: _description_
+        :type app_branch: Optional[ColumnMapElement]
+        :param log_version: _description_
+        :type log_version: Optional[ColumnMapElement]
+        :param timestamp: _description_
+        :type timestamp: Optional[ColumnMapElement]
+        :param time_offset: _description_
+        :type time_offset: Optional[ColumnMapElement]
+        :param event_sequence_index: _description_
+        :type event_sequence_index: Optional[ColumnMapElement]
+        :param event_name: _description_
+        :type event_name: Optional[ColumnMapElement]
+        :param event_source: _description_
+        :type event_source: Optional[ColumnMapElement]
+        :param event_data: _description_
+        :type event_data: Optional[ColumnMapElement]
+        :param game_state: _description_
+        :type game_state: Optional[ColumnMapElement]
+        :param user_data: _description_
+        :type user_data: Optional[ColumnMapElement]
+        :param other_elements: _description_, defaults to None
+        :type other_elements: Optional[Map], optional
         """
         unparsed_elements : Map = other_elements or {}
 
+        self._app_version          : ColumnMapElement = app_version          or self._parseAppVersion(unparsed_elements=self._raw_map)
+        self._app_branch           : ColumnMapElement = app_branch           or self._parseAppBranch(unparsed_elements=self._raw_map)
+        self._log_version          : ColumnMapElement = log_version          or self._parseLogVersion(unparsed_elements=self._raw_map)
         self._timestamp            : ColumnMapElement = timestamp            or self._parseTimestamp(unparsed_elements=unparsed_elements)
         self._time_offset          : ColumnMapElement = time_offset          or self._parseTimeoffset(unparsed_elements=unparsed_elements)
         self._event_sequence_index : ColumnMapElement = event_sequence_index or self._parseSequenceIndex(unparsed_elements=unparsed_elements)
@@ -53,8 +98,19 @@ class EventMapSchema(ColumnMapSchema):
         self._user_data            : ColumnMapElement = user_data            or self._parseUserData(unparsed_elements=unparsed_elements)
 
         super().__init__(name=name, app_id=app_id, user_id=user_id, session_id=session_id,
-                         app_version=app_version, app_branch=app_branch, log_version=log_version,
                          other_elements=unparsed_elements)
+
+    @property
+    def AppVersionColumn(self) -> ColumnMapElement:
+        return self._app_version
+
+    @property
+    def AppBranchColumn(self) -> ColumnMapElement:
+        return self._app_branch
+
+    @property
+    def LogVersionColumn(self) -> ColumnMapElement:
+        return self._log_version
 
     @property
     def TimestampColumn(self) -> ColumnMapElement:
@@ -94,16 +150,102 @@ class EventMapSchema(ColumnMapSchema):
     def Default(cls) -> "EventMapSchema":
         return EventMapSchema(
             name="DefaultEventTableSchema",
-            column_map={},
-            columns=cls._DEFAULT_COLUMNS,
+            app_id="app_id",
+            user_id="user_id",
+            session_id="session_id",
+            app_version="app_version",
+            app_branch="app_branch",
+            log_version="log_version",
+            timestamp="timestamp",
+            time_offset="time_offset",
+            event_sequence_index="event_sequence_index",
+            event_name="event_name",
+            event_source="event_source",
+            event_data="event_data",
+            game_state="game_state",
+            user_data="user_data",
             other_elements={}
         )
+
+    @classmethod
+    def _fromDict(cls, name:str, unparsed_elements:Map, key_overrides:Optional[Dict[str, str]]=None, default_override:Optional[Self]=None)-> "EventMapSchema":
+        """Function to generate an EventMapSchema from a dictionary.
+
+        The structure is assumed to be as follows:
+        ```python
+        {
+            "session_id"           : "session_id",
+            "app_id"               : null,
+            "timestamp"            : "client_time",
+            "event_name"           : "event_name",
+            "event_data"           : "event_data",
+            "event_source"         : "event_source",
+            "app_version"          : "app_version",
+            "app_branch"           : "app_branch",
+            "log_version"          : "log_version",
+            "time_offset"          : "client_offset",
+            "user_id"              : "user_id",
+            "user_data"            : "user_data",
+            "game_state"           : "game_state",
+            "event_sequence_index" : "event_sequence_index"
+        }
+        ```
+
+        The specific handling of the column map will be determined by the specific TableSchema subclass on which the FromDict feature is called.
+
+        :param name: _description_
+        :type name: str
+        :param unparsed_elements: _description_
+        :type unparsed_elements: Map
+        :param key_overrides: _description_, defaults to None
+        :type key_overrides: Optional[Dict[str, str]], optional
+        :param default_override: _description_, defaults to None
+        :type default_override: Optional[Self], optional
+        :return: _description_
+        :rtype: EventMapSchema
+        """
+        return EventMapSchema(name=name,
+                              app_id=None, user_id=None, session_id=None,
+                              app_version=None, app_branch=None, log_version=None,
+                              timestamp=None, time_offset=None, event_sequence_index=None,
+                              event_name=None, event_source=None, event_data=None,
+                              game_state=None, user_data=None, other_elements=unparsed_elements)
 
     # *** PUBLIC STATICS ***
 
     # *** PUBLIC METHODS ***
 
     # *** PRIVATE STATICS ***
+
+    @staticmethod
+    def _parseAppVersion(unparsed_elements:Map) -> Optional[ColumnMapElement]:
+        return ColumnMapSchema.ParseElement(
+            unparsed_elements=unparsed_elements,
+            valid_keys=["app_version"],
+            to_type=[str, list, dict],
+            default_value=None,
+            remove_target=False
+        )
+
+    @staticmethod
+    def _parseAppBranch(unparsed_elements:Map) -> Optional[ColumnMapElement]:
+        return ColumnMapSchema.ParseElement(
+            unparsed_elements=unparsed_elements,
+            valid_keys=["app_branch", "app_flavor"],
+            to_type=[str, list, dict],
+            default_value=None,
+            remove_target=False
+        )
+
+    @staticmethod
+    def _parseLogVersion(unparsed_elements:Map) -> Optional[ColumnMapElement]:
+        return ColumnMapSchema.ParseElement(
+            unparsed_elements=unparsed_elements,
+            valid_keys=["log_version"],
+            to_type=[str, list, dict],
+            default_value=None,
+            remove_target=False
+        )
 
     @staticmethod
     def _parseTimestamp(unparsed_elements:Map) -> Optional[ColumnMapElement]:
