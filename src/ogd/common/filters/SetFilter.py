293b6f914304300c4a12f99854dfbf607c605cd1
Luke Swanson <lwswanson2@wisc.edu>
2025-07-29 22:00:43 -0500

    Fix for missed conflict resolution>

--- src/ogd/common/filters/SetFilter.py
+++ src/ogd/common/filters/SetFilter.py
@@ -1,36 +1,11 @@
 ## import standard libraries
 import logging
-<<<<<<< HEAD
-from typing import Any
-=======
 from typing import Any, Generic, Set, TypeVar
->>>>>>> main
 # import local files
 from ogd.common.utils.Logger import Logger
 from ogd.common.filters.Filter import Filter
 from ogd.common.models.enums.FilterMode import FilterMode
 
-<<<<<<< HEAD
-class SetFilter(Filter):
-    def __init__(self, mode:FilterMode, set_elements:Any):
-        super().__init__(mode=mode)
-        if len(set_elements) == 0:
-            Logger.Log("Creating a SetFilter from an empty set! This will be intepreted as filtering out all values, not as an empty filter that allows all values to pass! For the latter case, use the NoFilter class.", logging.WARNING)
-        self._set = set(set_elements)
-
-    def __str__(self) -> str:
-        _exclude_clause = "not in " if self.FilterMode == FilterMode.EXCLUDE else ""
-        return f"{_exclude_clause}set of {len(self._set)} elements"
-    
-    def __repr__(self) -> str:
-        _types = set(type(elem).__name__ for elem in self.Set)
-        _type_str = " | ".join(_types)
-        return f"<class {type(self).__name__} {self.FilterMode}:Set[{_type_str}] with {len(self.Set)} elements>"
-
-    @property
-    def Set(self):
-        return self._set
-=======
 T = TypeVar("T")
 class SetFilter(Filter[T]):
     def __init__(self, mode:FilterMode, set_elements:Set[T]=set()):
@@ -64,4 +39,3 @@ class SetFilter(Filter[T]):
     @property
     def Max(self) -> None:
         return None
->>>>>>> main
