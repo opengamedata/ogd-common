d632c0ce2fce9e21099fb553e44d1a6e739a281b
Luke Swanson <lwswanson2@wisc.edu>
2025-07-29 21:59:48 -0500

    Accidentally added some filter classes back in during merge.

--- src/ogd/common/filters/NoFilter.py
+++
@@ -1,9 +0,0 @@
-from ogd.common.filters.Filter import Filter
-from ogd.common.models.enums.FilterMode import FilterMode
-
-class NoFilter(Filter):
-    def __init__(self):
-        super().__init__(mode=FilterMode.NOFILTER)
-
-    def __str__(self) -> str:
-        return f"no filter"
