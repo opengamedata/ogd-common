218c7302189c5bf8afd9939898eb6d2e360c8125
Luke Swanson <lwswanson2@wisc.edu>
2025-07-30 18:43:47 -0500

    Initial work to set up a testbed for BQ interface.

---
+++ tests/cases/storage/interfaces/test_BigQueryInterface.py
@@ -0,0 +1,76 @@
+# import libraries
+import logging
+import unittest
+from unittest import TestCase
+# import 3rd-party libraries
+from google.cloud import bigquery
+# import ogd libraries.
+from ogd.common.configs.storage.BigQueryConfig import BigQueryConfig
+from ogd.common.configs.GameStoreConfig import GameStoreConfig
+from ogd.common.configs.TestConfig import TestConfig
+from ogd.common.schemas.tables.TableSchema import TableSchema
+from ogd.common.storage.connectors.BigQueryConnector import BigQueryConnector
+from ogd.common.storage.interfaces.BigQueryInterface import BigQueryInterface
+from ogd.common.utils.Logger import Logger
+# import locals
+from tests.config.t_config import settings
+
+class test_BigQueryInterface(TestCase):
+    """Testbed for the GameStoreConfig class.
+
+        TODO : Test more 'enabled' options/combinations.
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        # 1. Get testing config
+        _testing_cfg : TestConfig = TestConfig.FromDict(name="BQTestConfig", unparsed_elements=settings)
+        _level     = logging.DEBUG if _testing_cfg.Verbose else logging.INFO
+        Logger.std_logger.setLevel(_level)
+
+        # 2. Set up local instance of testing class
+        _elems = {
+            "DB_TYPE"    : "BIGQUERY",
+            "PROJECT_ID" : "wcer-field-day-ogd-1798",
+            "PROJECT_KEY": "./tests/config/ogd.json"
+        }
+        store_config = BigQueryConfig.FromDict(name="OPENGAMEDATA_BQ", unparsed_elements=_elems)
+
+        # table_schema = 
+
+        _elems = { "source":"OPENGAMEDATA_BQ", "database":"aqualab", "table":"aqualab_daily", "schema":"OPENGAMEDATA_BIGQUERY" }
+        config = GameStoreConfig.FromDict(name="BQStoreConfig", unparsed_elements=_elems)
+        config.StoreConfig = store_config
+
+        cls.test_interface = BigQueryInterface(config=config, fail_fast=True, store=None)
+
+    @staticmethod
+    def RunAll():
+        pass
+
+    def test_Open(self):
+        success = self.test_connector.Open()
+        self.assertIsInstance(success, bool)
+        self.assertTrue(success)
+
+    def test_Close(self):
+        self.test_connector.Open()
+        success = self.test_connector.Close()
+        self.assertIsInstance(success, bool)
+        self.assertTrue(success)
+
+    def test_IsOpen(self):
+        success = self.test_connector.Open()
+        is_open = self.test_connector.IsOpen
+        self.assertIsInstance(is_open, bool)
+        self.assertEqual(is_open, success)
+
+    def test_Client(self):
+        client = self.test_connector.Client
+        self.assertIsNone(client)
+        self.test_connector.Open()
+        client = self.test_connector.Client
+        self.assertIsInstance(client, bigquery.Client)
+
+if __name__ == '__main__':
+    unittest.main()
