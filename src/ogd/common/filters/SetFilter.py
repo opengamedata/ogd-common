a6682c18cd42ea108fb1a68f4ed47010d8a3b2eb
Luke Swanson <lwswanson2@wisc.edu>
2025-07-29 19:47:55 -0500

    A few more tweaks to SetFilter, trying to make it properly generic.

--- src/ogd/common/filters/SetFilter.py
+++ src/ogd/common/filters/SetFilter.py
@@ -1,21 +1,27 @@
 ## import standard libraries
 import logging
-from typing import Any, Set
+from typing import Any, Generic, Set, TypeVar
 # import local files
 from ogd.common.utils.Logger import Logger
 from ogd.common.filters.Filter import Filter
 from ogd.common.models.enums.FilterMode import FilterMode
 
-class SetFilter(Filter[Any]):
-    def __init__(self, mode:FilterMode, set_elements:Any):
+T = TypeVar("T")
+class SetFilter(Filter, Generic[T]):
+    def __init__(self, mode:FilterMode, set_elements:Set[T]=set()):
         super().__init__(mode=mode)
-        if len(set_elements) == 0:
-            Logger.Log("Creating a SetFilter from an empty set! This will be intepreted as filtering out all values, not as an empty filter that allows all values to pass! For the latter case, use the NoFilter class.", logging.WARNING)
         self._set = set(set_elements)
 
     def __str__(self) -> str:
-        _exclude_clause = "not in " if self.FilterMode == FilterMode.EXCLUDE else ""
-        return f"{_exclude_clause}set of {len(self._set)} elements"
+        ret_val : str
+
+        match self.FilterMode:
+            case FilterMode.EXCLUDE:
+                ret_val = f"not in set of {len(self._set)} elements"
+            case FilterMode.INCLUDE:
+                ret_val = f"in set of {len(self._set)} elements"
+        
+        return ret_val
     
     def __repr__(self) -> str:
         _types = set(type(elem).__name__ for elem in self.AsSet)
@@ -23,7 +29,7 @@ class SetFilter(Filter[Any]):
         return f"<class {type(self).__name__} {self.FilterMode}:Set[{_type_str}] with {len(self.AsSet)} elements>"
 
     @property
-    def AsSet(self) -> Set[Any]:
+    def AsSet(self) -> Set[T]:
         return self._set
 
     @property
