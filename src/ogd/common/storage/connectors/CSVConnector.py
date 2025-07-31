320d156acf72044621c2ea0ad6366ce377b4761f
Luke Swanson <lwswanson2@wisc.edu>
2025-07-31 16:52:12 -0500

    Make interface responsible for calling read on the connector file.

--- src/ogd/common/storage/connectors/CSVConnector.py
+++ src/ogd/common/storage/connectors/CSVConnector.py
@@ -1,9 +1,5 @@
 import logging
-from collections import defaultdict
-from typing import Optional
-# 3rd-party imports
-import numpy as np
-import pandas as pd
+from typing import Optional, IO
 ## import local files
 from ogd.common.configs.storage.FileStoreConfig import FileStoreConfig
 from ogd.common.schemas.tables.TableSchema import TableSchema
@@ -14,14 +10,12 @@ class CSVConnector(StorageConnector):
 
     # *** BUILT-INS & PROPERTIES ***
 
-    def __init__(self, config:FileStoreConfig, delim:str = ',', table:Optional[TableSchema]=None):
+    def __init__(self, config:FileStoreConfig, delim:str = ',', with_secondary_files:bool=False):
         # set up data from params
         super().__init__()
         self._config = config
-        self._table = table
         self._delimiter = delim
-        # set up data from file
-        self._data      : pd.DataFrame = pd.DataFrame()
+        self._file = None
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
 
@@ -30,39 +24,21 @@ class CSVConnector(StorageConnector):
         return self._config
 
     @property
-    def DataFrame(self) -> pd.DataFrame:
-        return self._data
+    def File(self) -> Optional[IO]:
+        return self._file
 
     def _open(self) -> bool:
         try:
-            # TODO should include option for access to the TableConfig in the interface, because obviously it should know what form the table takes.
-            target_types = defaultdict(str, {
-                column.Name : np.dtype(column.ValueType if column.ValueType in {"str", "int", "float"} else "object")
-                for column in self._table.Columns
-            }) if self._table is not None else None
-
-            date_columns = [
-                column.Name for column in self._table.Columns if column.ValueType in {"datetime", "timezone"}
-            ] if self._table is not None else []
-
-            self._data = pd.read_csv(
-                filepath_or_buffer=self.StoreConfig.Location.Filepath,
-                delimiter=self._delimiter,
-                dtype=target_types,
-                parse_dates=date_columns
-            )
-            # _data = pd.read_csv(filepath_or_buffer=self._filepath, delimiter=self._delimiter)
-            Logger.Log(f"Loaded from CSV, columns are: {self._data.dtypes}", logging.INFO)
-            Logger.Log(f"First few rows are:\n{self._data.head(n=3)}")
-            # self._data = _data.where(_data.notnull(), None)
-            self._is_open = True
-            return True
+            self._file = open(self.StoreConfig.Filepath, "rw")
         except FileNotFoundError as err:
             Logger.Log(f"Could not find file {self.StoreConfig.Filepath}.", logging.ERROR)
             return False
+        else:
+            return True
 
     def _close(self) -> bool:
-        self._data = pd.DataFrame() # make new dataframe, let old data get garbage collected I assume.
+        if self.File:
+            self.File.close()
         self._is_open = False
         return True
 
--- src/ogd/common/storage/interfaces/CSVInterface.py
+++ src/ogd/common/storage/interfaces/CSVInterface.py
@@ -1,7 +1,11 @@
 import logging
 import pandas as pd
+from collections import defaultdict
 from datetime import datetime
 from typing import Dict, List, Tuple, Optional, Union
+# 3rd-party imports
+import numpy as np
+import pandas as pd
 ## import local files
 from ogd.common.filters import *
 from ogd.common.filters.collections import *
@@ -13,6 +17,7 @@ from ogd.common.models.enums.VersionType import VersionType
 from ogd.common.models.SemanticVersion import SemanticVersion
 from ogd.common.storage.interfaces.Interface import Interface
 from ogd.common.storage.connectors.CSVConnector import CSVConnector
+from ogd.common.utils.Logger import Logger
 
 type PDMask = Union[pd.Series, bool]
 class CSVInterface(Interface):
@@ -23,12 +28,38 @@ class CSVInterface(Interface):
         self._store : CSVConnector
 
         super().__init__(config=config, fail_fast=fail_fast)
+        self._delimiter = delim
+        self._data = pd.DataFrame()
         if store:
             self._store = store
         elif isinstance(self.Config.StoreConfig, FileStoreConfig):
-            self._store = CSVConnector(config=self.Config.StoreConfig, delim=delim, table=self.Config.Table)
-        # set up data from file
+            self._store = CSVConnector(config=self.Config.StoreConfig, delim=delim, with_secondary_files=False)
+
+        # We always just read the file right away.
         self.Connector.Open()
+        if self.Connector.IsOpen and self.Connector.File:
+            # TODO should include option for access to the TableConfig in the interface, because obviously it should know what form the table takes.
+            target_types = defaultdict(str, {
+                column.Name : np.dtype(column.ValueType if column.ValueType in {"str", "int", "float"} else "object")
+                for column in self.Config.Table.Columns
+            }) if self.Config.Table is not None else None
+
+            date_columns = [
+                column.Name for column in self.Config.Table.Columns if column.ValueType in {"datetime", "timezone"}
+            ] if self.Config.Table is not None else []
+
+            self._data = pd.read_csv(
+                filepath_or_buffer=self.Connector.File,
+                delimiter=self._delimiter,
+                dtype=target_types,
+                parse_dates=date_columns
+            )
+            Logger.Log(f"Loaded from CSV, columns are: {self._data.dtypes}", logging.INFO)
+            Logger.Log(f"First few rows are:\n{self._data.head(n=3)}")
+
+    @property
+    def DataFrame(self) -> pd.DataFrame:
+        return self._data
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
 
@@ -39,8 +70,8 @@ class CSVInterface(Interface):
     def _availableIDs(self, mode:IDMode, date_filter:TimingFilterCollection, version_filter:VersioningFilterCollection) -> List[str]:
         ret_val : List[str] = []
 
-        if not self.Connector.DataFrame.empty:
-            server_times = pd.to_datetime(self.Connector.DataFrame['server_time'])
+        if not self.DataFrame.empty:
+            server_times = pd.to_datetime(self.DataFrame['server_time'])
             mask = None
             if date_filter.TimestampFilter:
                 if date_filter.TimestampFilter.Min and date_filter.TimestampFilter.Max:
@@ -51,7 +82,7 @@ class CSVInterface(Interface):
                     mask = server_times <= date_filter.TimestampFilter.Max
             # if versions is not None and versions is not []:
             #     mask = mask & (self._data['app_version'].isin(versions))
-            data_masked = self.Connector.DataFrame.loc[mask] if mask is not None else self.Connector.DataFrame
+            data_masked = self.DataFrame.loc[mask] if mask is not None else self.DataFrame
             ret_val = [str(id) for id in data_masked['session_id'].unique().tolist()]
 
         return ret_val
@@ -64,18 +95,18 @@ class CSVInterface(Interface):
             if id_filter.SessionFilter:
                 match id_filter.SessionFilter.FilterMode:
                     case FilterMode.INCLUDE:
-                        sess_mask = self.Connector.DataFrame['session_id'].isin(id_filter.SessionFilter.AsSet)
+                        sess_mask = self.DataFrame['session_id'].isin(id_filter.SessionFilter.AsSet)
                     case FilterMode.EXCLUDE:
-                        sess_mask = ~self.Connector.DataFrame['session_id'].isin(id_filter.SessionFilter.AsSet)
+                        sess_mask = ~self.DataFrame['session_id'].isin(id_filter.SessionFilter.AsSet)
             user_mask : PDMask = True
             if id_filter.PlayerFilter:
                 match id_filter.PlayerFilter.FilterMode:
                     case FilterMode.INCLUDE:
-                        user_mask = self.Connector.DataFrame['user_id'].isin(id_filter.PlayerFilter.AsSet)
+                        user_mask = self.DataFrame['user_id'].isin(id_filter.PlayerFilter.AsSet)
                     case FilterMode.EXCLUDE:
-                        user_mask = ~self.Connector.DataFrame['user_id'].isin(id_filter.PlayerFilter.AsSet)
+                        user_mask = ~self.DataFrame['user_id'].isin(id_filter.PlayerFilter.AsSet)
 
-            _col  = self.Connector.DataFrame[sess_mask & user_mask]['timestamp']
+            _col  = self.DataFrame[sess_mask & user_mask]['timestamp']
             min_date = _col.min()
             max_date = _col.max()
             ret_val = {'min':pd.to_datetime(min_date), 'max':pd.to_datetime(max_date)}
@@ -87,7 +118,7 @@ class CSVInterface(Interface):
 
         if self.Connector.IsOpen:
             version_col  : str = "log_version" if mode==VersionType.LOG else "app_version" if mode==VersionType.APP else "app_branch"
-            ret_val = [SemanticVersion.FromString(str(ver)) for ver in self.Connector.DataFrame[version_col].unique().tolist()]
+            ret_val = [SemanticVersion.FromString(str(ver)) for ver in self.DataFrame[version_col].unique().tolist()]
 
         return ret_val
 
@@ -95,29 +126,29 @@ class CSVInterface(Interface):
     def _getEventRows(self, id_filter:IDFilterCollection, date_filter:TimingFilterCollection, version_filter:VersioningFilterCollection, event_filter:EventFilterCollection) -> List[Tuple]:
         ret_val : List[Tuple] = []
 
-        if self.Connector.IsOpen and not self.Connector.DataFrame.empty:
+        if self.Connector.IsOpen and not self.DataFrame.empty:
             sess_mask : PDMask = True
             if id_filter.SessionFilter:
                 match id_filter.SessionFilter.FilterMode:
                     case FilterMode.INCLUDE:
-                        sess_mask = self.Connector.DataFrame['session_id'].isin(id_filter.SessionFilter.AsSet)
+                        sess_mask = self.DataFrame['session_id'].isin(id_filter.SessionFilter.AsSet)
                     case FilterMode.EXCLUDE:
-                        sess_mask = ~self.Connector.DataFrame['session_id'].isin(id_filter.SessionFilter.AsSet)
+                        sess_mask = ~self.DataFrame['session_id'].isin(id_filter.SessionFilter.AsSet)
             user_mask : PDMask = True
             if id_filter.PlayerFilter:
                 match id_filter.PlayerFilter.FilterMode:
                     case FilterMode.INCLUDE:
-                        user_mask = self.Connector.DataFrame['user_id'].isin(id_filter.PlayerFilter.AsSet)
+                        user_mask = self.DataFrame['user_id'].isin(id_filter.PlayerFilter.AsSet)
                     case FilterMode.EXCLUDE:
-                        user_mask = ~self.Connector.DataFrame['user_id'].isin(id_filter.PlayerFilter.AsSet)
+                        user_mask = ~self.DataFrame['user_id'].isin(id_filter.PlayerFilter.AsSet)
             event_mask : PDMask = True
             if event_filter.EventNameFilter:
                 match event_filter.EventNameFilter.FilterMode:
                     case FilterMode.INCLUDE:
-                        event_mask = self.Connector.DataFrame['event_name'].isin(event_filter.EventNameFilter.AsSet)
+                        event_mask = self.DataFrame['event_name'].isin(event_filter.EventNameFilter.AsSet)
                     case FilterMode.EXCLUDE:
-                        event_mask = ~self.Connector.DataFrame['event_name'].isin(event_filter.EventNameFilter.AsSet)
-            _data = self.Connector.DataFrame[sess_mask & user_mask & event_mask]
+                        event_mask = ~self.DataFrame['event_name'].isin(event_filter.EventNameFilter.AsSet)
+            _data = self.DataFrame[sess_mask & user_mask & event_mask]
             ret_val = list(_data.itertuples(index=False, name=None))
         return ret_val
 
