f3cc672b5fbdd92e7a02d5006e763b4a1cc4c62a
Luke Swanson <lwswanson2@wisc.edu>
2025-08-03 18:43:54 -0500

    Remake FilterCollection as DatasetFilterCollection.

---
+++ src/ogd/common/filters/collections/DatasetFilterCollection.py
@@ -0,0 +1,36 @@
+## import standard libraries
+import abc
+from typing import Any, Dict
+# import local files
+from ogd.common.filters.Filter import Filter
+from ogd.common.filters.collections.EventFilterCollection import EventFilterCollection
+from ogd.common.filters.collections.IDFilterCollection import IDFilterCollection
+from ogd.common.filters.collections.SequencingFilterCollection import SequencingFilterCollection
+from ogd.common.filters.collections.VersioningFilterCollection import VersioningFilterCollection
+
+class DatasetFilterCollection:
+    def __init__(self,
+                 id_filters:IDFilterCollection=IDFilterCollection(),
+                 sequence_filters:SequencingFilterCollection=SequencingFilterCollection(),
+                 version_filters:VersioningFilterCollection=VersioningFilterCollection(),
+                 event_filters:EventFilterCollection=EventFilterCollection()):
+        self._id_filters = id_filters
+        self._sequence_filters = sequence_filters
+        self._version_filters=version_filters
+        self._event_filters = event_filters
+
+    @property
+    def IDFilters(self):
+        return self._id_filters
+    
+    @property
+    def Sequences(self):
+        return self._sequence_filters
+    
+    @property
+    def Versions(self):
+        return self._version_filters
+    
+    @property
+    def Events(self):
+        return self._event_filters
