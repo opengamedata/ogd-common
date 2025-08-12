740104c5e0931b9097ed18062ba11d2382bd0829
Luke Swanson <lwswanson2@wisc.edu>
2025-08-12 11:17:21 -0500

    Add placeholder comment for someday allowing interface to firebase-based BQ.

--- src/ogd/common/storage/interfaces/InterfaceFactory.py
+++ src/ogd/common/storage/interfaces/InterfaceFactory.py
@@ -4,6 +4,7 @@ from ogd.common.configs.GameStoreConfig import GameStoreConfig
 from ogd.common.storage.interfaces.Interface import Interface
 from ogd.common.storage.interfaces.CSVInterface import CSVInterface
 from ogd.common.storage.interfaces.BigQueryInterface import BigQueryInterface
+from ogd.common.storage.interfaces.BQFirebaseInterface import BQFirebaseInterface
 from ogd.common.storage.interfaces.MySQLInterface import MySQLInterface
 from ogd.common.schemas.tables.TableSchema import TableSchema
 from ogd.common.schemas.tables.EventTableSchema import EventTableSchema
@@ -15,7 +16,9 @@ class InterfaceFactory:
         if config.StoreConfig:
             match (config.StoreConfig.Type.upper()):
                 # case "MYSQL":
-                #     return MySQLInterface(schema=config, fail_fast=fail_fast)
+                #     return MySQLInterface(config=config, fail_fast=fail_fast)
+                # case "FIREBASE":
+                #     return BQFirebaseInterface(config=config, fail_fast=fail_fast)
                 case "BIGQUERY":
                     return BigQueryInterface(config=config, fail_fast=fail_fast)
                 case "FILE" | "CSV" | "TSV":
