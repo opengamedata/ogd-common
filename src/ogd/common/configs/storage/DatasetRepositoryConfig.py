80b599d38e04e660b7c8936e93435dc8a37639be
Luke Swanson <lwswanson2@wisc.edu>
2025-08-14 00:05:21 -0500

    DatasetRepo config now has nicer handling of input types.

--- src/ogd/common/configs/storage/DatasetRepositoryConfig.py
+++ src/ogd/common/configs/storage/DatasetRepositoryConfig.py
@@ -49,10 +49,10 @@ class DatasetRepositoryConfig(DataStoreConfig):
                  # dict of leftovers
                  other_elements:Optional[Map]=None
         ):
-        unparsed_elements : Map = other_elements or {}
+        fallbacks : Map = other_elements or {}
 
-        self._indexing : RepositoryIndexingConfig           = indexing or self._parseIndexingConfig(unparsed_elements=unparsed_elements)
-        self._datasets : Dict[str, DatasetCollectionSchema] = datasets or self._parseDatasets(unparsed_elements=unparsed_elements)
+        self._indexing : RepositoryIndexingConfig           = self._toIndexingConfig(indexing=indexing, fallbacks=fallbacks)
+        self._datasets : Dict[str, DatasetCollectionSchema] = datasets or self._parseDatasets(unparsed_elements=fallbacks)
         super().__init__(name=name, store_type="Repository", other_elements=other_elements)
 
     def __str__(self) -> str:
@@ -138,6 +138,19 @@ class DatasetRepositoryConfig(DataStoreConfig):
     # *** PUBLIC METHODS ***
 
     # *** PRIVATE STATICS ***
+
+    @staticmethod
+    def _toIndexingConfig(indexing:Optional[RepositoryIndexingConfig | Map | str], fallbacks:Map) -> RepositoryIndexingConfig:
+        ret_val : RepositoryIndexingConfig
+        if isinstance(indexing, RepositoryIndexingConfig):
+            ret_val = indexing
+        elif isinstance(indexing, dict):
+            ret_val = RepositoryIndexingConfig.FromDict(name="DatasetRepositoryIndex", unparsed_elements=fallbacks)
+        elif isinstance(indexing, str):
+            ret_val = RepositoryIndexingConfig(name="DatasetRepositoryIndex", local_dir=indexing, remote_url=None, templates_url=None)
+        else:
+            ret_val = DatasetRepositoryConfig._parseIndexingConfig(unparsed_elements=fallbacks)
+        return ret_val
 
     @staticmethod
     def _parseIndexingConfig(unparsed_elements:Map) -> RepositoryIndexingConfig:
