f4def0ba1dee2022bd43934a64a05cddc35d5f50
Luke Swanson <lwswanson2@wisc.edu>
2025-08-14 00:14:14 -0500

    Fix a type hint.

--- src/ogd/common/schemas/locations/RAMLocationSchema.py
+++ src/ogd/common/schemas/locations/RAMLocationSchema.py
@@ -39,7 +39,7 @@ class RAMLocationSchema(LocationSchema):
         )
 
     @classmethod
-    def _fromDict(cls, name:str, unparsed_elements:Map, key_overrides:Optional[Dict[str, str]]=None, default_override:Optional[Self]=None)-> "FileLocationSchema":
+    def _fromDict(cls, name:str, unparsed_elements:Map, key_overrides:Optional[Dict[str, str]]=None, default_override:Optional[Self]=None)-> "RAMLocationSchema":
         """Create a DatabaseLocationSchema from a given dictionary
 
         TODO : Add example of what format unparsed_elements is expected to have.
