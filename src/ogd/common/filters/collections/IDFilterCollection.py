ebf9d663b3fc87677c551e0b8a3206c64cb4c485
Luke Swanson <lwswanson2@wisc.edu>
2025-08-19 11:09:31 -0500

    With nicer ways of initializing SetFilters, simplify how we set new sets for ID filters, while also expanding accepted types.

--- src/ogd/common/filters/collections/IDFilterCollection.py
+++ src/ogd/common/filters/collections/IDFilterCollection.py
@@ -1,5 +1,5 @@
 ## import standard libraries
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Tuple
 # import local files
 from ogd.common.filters import *
 from ogd.common.models.enums.FilterMode import FilterMode
@@ -9,25 +9,29 @@ class IDFilterCollection:
     """
     def __init__(self,
                  session_filter : Optional[SetFilter[str] | NoFilter] = None,
-                 player_filter  : Optional[SetFilter[str] | NoFilter] = None):
+                 player_filter  : Optional[SetFilter[str] | NoFilter] = None,
+                 app_filter     : Optional[SetFilter[str] | NoFilter] = None):
         self._session_filter : SetFilter[str] | NoFilter = session_filter or NoFilter()
         self._player_filter  : SetFilter[str] | NoFilter = player_filter  or NoFilter()
+        self._app_filter     : SetFilter[str] | NoFilter = app_filter     or NoFilter()
 
     def __str__(self) -> str:
         ret_val = "no versioning filters"
         if self.Sessions or self.Players:
+            _app_str = f"app(s) {self.AppIDs}" if self.AppIDs else None
             _sess_str = f"session(s) {self.Sessions}" if self.Sessions else None
             _ply_str = f"player(s) {self.Players}" if self.Players else None
-            _ver_strs = ", ".join([elem for elem in [_sess_str, _ply_str] if elem is not None])
+            _ver_strs = ", ".join([elem for elem in [_app_str, _sess_str, _ply_str] if elem is not None])
             ret_val = f"event filters: {_ver_strs}"
         return ret_val
 
     def __repr__(self) -> str:
         ret_val = f"<class {type(self).__name__} no filters>"
         if self.Sessions or self.Players:
+            _app_str = f"app(s) {self.AppIDs}" if self.AppIDs else None
             _sess_str = f"session(s) {self.Sessions}" if self.Sessions else None
             _ply_str = f"player(s) {self.Players}" if self.Players else None
-            _ver_strs = " ^ ".join([elem for elem in [_sess_str, _ply_str] if elem is not None])
+            _ver_strs = " ^ ".join([elem for elem in [_app_str, _sess_str, _ply_str] if elem is not None])
             ret_val = f"<class {type(self).__name__} {_ver_strs}>"
         return ret_val
 
@@ -35,25 +39,22 @@ class IDFilterCollection:
     def Sessions(self) -> Filter[str]:
         return self._session_filter
     @Sessions.setter
-    def Sessions(self, included_sessions:Optional[SetFilter | List[str] | Set[str]]) -> None:
-        if included_sessions is None:
-            self._session_filter = NoFilter()
-        elif isinstance(included_sessions, SetFilter):
-            self._session_filter = included_sessions
-        elif isinstance(included_sessions, list) or isinstance(included_sessions, set):
-            self._session_filter = SetFilter(mode=FilterMode.INCLUDE, set_elements=set(included_sessions))
+    def Sessions(self, included_sessions:Optional[SetFilter | Set[str] | List[str] | Tuple[str] | str]) -> None:
+        self._session_filter = SetFilter[str](mode=self.Sessions.FilterMode, set_elements=included_sessions)
 
     @property
     def Players(self) -> Filter[str]:
         return self._player_filter
     @Players.setter
-    def Players(self, included_players:Optional[SetFilter | List[str] | Set[str]]) -> None:
-        if included_players is None:
-            self._player_filter = NoFilter()
-        elif isinstance(included_players, SetFilter):
-            self._player_filter = included_players
-        elif isinstance(included_players, list) or isinstance(included_players, set):
-            self._player_filter = SetFilter(mode=FilterMode.INCLUDE, set_elements=set(included_players))
+    def Players(self, included_players:Optional[SetFilter | Set[str] | List[str] | Tuple[str] | str]) -> None:
+        self._player_filter = SetFilter[str](mode=self.Players.FilterMode, set_elements=included_players)
+
+    @property
+    def AppIDs(self) -> Filter[str]:
+        return self._app_filter
+    @AppIDs.setter
+    def AppIDs(self, included_apps:Optional[SetFilter | Set[str] | List[str] | Tuple[str] | str]) -> None:
+        self._app_filter = SetFilter[str](mode=self.AppIDs.FilterMode, set_elements=included_apps)
 
     @property
     def any(self) -> bool:
