f285fd62078825d75bc1d9756d4a281cacc0f6c8
Luke Swanson <lwswanson2@wisc.edu>
2025-08-20 12:05:21 -0500

    Add option to load a table from a file.

--- src/ogd/common/schemas/tables/TableSchemaFactory.py
+++ src/ogd/common/schemas/tables/TableSchemaFactory.py
@@ -1,12 +1,28 @@
+from pathlib import Path
 from typing import Any, Dict
 
 from ogd.common.schemas.tables.TableSchema import TableSchema
 from ogd.common.schemas.tables.EventTableSchema import EventTableSchema
 from ogd.common.schemas.tables.FeatureTableSchema import FeatureTableSchema
+from ogd.common.utils.fileio import loadJSONFile
 
 class TableSchemaFactory:
     @staticmethod
     def FromDict(name:str, all_elements:Dict[str, Any])-> TableSchema:
+        table_type = str(all_elements.get("table_type", "NOT FOUND"))
+        # _table_type       = TableType.FromString(_table_type_str) if _table_type_str is not None else TableType.EVENT
+        match (table_type.upper()):
+            case "EVENT":
+                return EventTableSchema.FromDict(name=name, unparsed_elements=all_elements)
+            case "FEATURE":
+                return FeatureTableSchema.FromDict(name=name, unparsed_elements=all_elements)
+            case _:
+                raise ValueError(f"Could not generate TableSchema from dictionary, table_type had invalid value {table_type}")
+
+    @staticmethod
+    def FromFile(name:str, filepath:Path|str)-> TableSchema:
+        filepath = filepath if isinstance(filepath, Path) else Path(filepath)
+        all_elements = loadJSONFile(filename=filepath.name, path=filepath.parent)
         table_type = str(all_elements.get("table_type", "NOT FOUND"))
         # _table_type       = TableType.FromString(_table_type_str) if _table_type_str is not None else TableType.EVENT
         match (table_type.upper()):
