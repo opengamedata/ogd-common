236277c8934a3873304cd22454a98eb7c1062c3c
Luke Swanson <lwswanson2@wisc.edu>
2026-02-25 15:18:21 -0600

    Small renaming, plus a new StaticCase test case.

--- tests/cases/configs/DataTableConfig/StaticCase.py
+++ tests/cases/configs/DataTableConfig/StaticCase.py
@@ -13,83 +13,23 @@ from ogd.common.utils.Logger import Logger
 from src.ogd.common.configs.DataTableConfig import DataTableConfig
 from tests.config.t_config import settings
 
-class test_DataTableConfig(TestCase):
-    """Testbed for the DataTableConfig class.
+class StaticCase(TestCase):
+    """DataTableConfig test case to check that all loading and parsing work correctly.
+
+    The test fixture here is technically "empty", since we don't define any data initially.
+    We're ultimately just testing all the static functions here, and the name "static" is slightly more descriptive than "empty," hence StaticCase.
     """
 
     @classmethod
     def setUpClass(cls) -> None:
         # 1. Get testing config
         _testing_cfg = TestConfig.FromDict(name="SchemaTestConfig", unparsed_elements=settings)
-        _level     = logging.DEBUG if _testing_cfg.Verbose else logging.INFO
+        _level       = logging.DEBUG if _testing_cfg.Verbose else logging.INFO
         Logger.std_logger.setLevel(_level)
-
-        # 2. Set up local instance of testing class
-        source_elems = {
-            "DB_TYPE"    : "BIGQUERY",
-            "PROJECT_ID" : "aqualab-project",
-            "PROJECT_KEY": {
-                "FILE": "key.txt",
-                "PATH": "./"
-            }
-        }
-        cls.test_schema = DataTableConfig(
-            name="Game Source Schema",
-            store="AQUALAB_BQ",
-            table_schema="OPENGAMEDATA_BIGQUERY",
-            table_location=DatabaseLocationSchema(name="DBLocation", database_name="aqualab", table_name="aqualab_daily"),
-            other_elements={ "foo":"bar" }
-        )
 
     @staticmethod
     def RunAll():
         pass
-
-    def test_Name(self):
-        _str = self.test_schema.Name
-        self.assertIsInstance(_str, str)
-        self.assertEqual(_str, "Game Source Schema")
-
-    def test_SourceName(self):
-        _str = self.test_schema.StoreName
-        self.assertIsInstance(_str, str)
-        self.assertEqual(_str, "AQUALAB_BQ")
-
-    @unittest.skip("Not yet implemented")
-    def test_Source(self):
-        pass
-        # _str = self.test_schema.SourceName
-        # self.assertIsInstance(_str, str)
-        # self.assertEqual(_str, "AQUALAB_BQ")
-
-    def test_DatabaseName(self):
-        _str = self.test_schema.DatabaseName
-        self.assertIsInstance(_str, str)
-        self.assertEqual(_str, "aqualab")
-
-    def test_TableName(self):
-        _str = self.test_schema.TableName
-        self.assertIsInstance(_str, str)
-        self.assertEqual(_str, "aqualab_daily")
-
-    def test_SchemaName(self):
-        """Test the correctness of TableConfig property
-        """
-        _str = self.test_schema.TableSchemaName
-        self.assertIsInstance(_str, str)
-        self.assertEqual(_str, "OPENGAMEDATA_BIGQUERY")
-
-    def test_NonStandardElements(self):
-        _elems = {
-            "foo":"bar"
-        }
-        self.assertIsInstance(self.test_schema.NonStandardElements, dict)
-        self.assertEqual(self.test_schema.NonStandardElements, _elems)
-
-    def test_NonStandardElementNames(self):
-        _elem_names = ["foo"]
-        self.assertIsInstance(self.test_schema.NonStandardElementNames, list)
-        self.assertEqual(self.test_schema.NonStandardElementNames, _elem_names)
 
     def test_FromDict(self):
         """Test case for whether the FromDict function is working properly.
@@ -129,11 +69,19 @@ class test_DataTableConfig(TestCase):
         _str = DataTableConfig._parseStoreName(_map)
         self.assertIsInstance(_str, str)
         self.assertEqual(_str, "Foo")
-        # First parse should remove key, so second should return default from class
+        self.assertNotIn("source", _map) # First parse should remove key, so "source" should not exist anymore.
+        
+    def test_parseSourceName_missing(self):
+        _map = {
+            "fakekey" : "Bar"
+        }
         _str = DataTableConfig._parseStoreName(_map)
         self.assertIsInstance(_str, str)
         self.assertEqual(_str, DataTableConfig._DEFAULT_STORE_NAME)
-        # Check that source_name is also treated as valid
+
+    def test_parseSourceName_alt_name(self):
+        """Check that `source_name` is also treated as valid key for the source name.
+        """
         _map = {
             "source_name":"Foo",
             "fakekey" : "Bar"
