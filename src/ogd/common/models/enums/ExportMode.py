3f0a119bade1116d3f3c4c5c523234912eae4f74
Luke Swanson <lwswanson2@wisc.edu>
2026-03-26 17:14:24 -0500

    Remove the aggregation modes from ExportMode.

--- src/ogd/common/models/enums/ExportMode.py
+++ src/ogd/common/models/enums/ExportMode.py
@@ -15,9 +15,6 @@ class ExportMode(IntEnum):
     EVENTS     = 1
     DETECTORS  = 2
     FEATURES   = 3
-    SESSION    = 4
-    PLAYER     = 5
-    POPULATION = 6
 
     def __str__(self):
         return self.name
