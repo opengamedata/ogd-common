a06ce99f62d374d0bf5adddfac5ec517c6b20617
Luke Swanson <lwswanson2@wisc.edu>
2026-03-26 17:07:27 -0500

    Add initial comment blocks explaining what the classes should be.

--- src/ogd/common/models/enums/ExportMode.py
+++ src/ogd/common/models/enums/ExportMode.py
@@ -2,6 +2,16 @@
 from enum import IntEnum
 
 class ExportMode(IntEnum):
+    """An enum representing the various modes that can be part of an OGD export.
+
+    These are just the kinds of data that OGD outputs:
+    Game Events, Generated (Detector) Events, Features, Models.
+
+    :param IntEnum: _description_
+    :type IntEnum: _type_
+    :return: _description_
+    :rtype: _type_
+    """
     EVENTS     = 1
     DETECTORS  = 2
     FEATURES   = 3
--- src/ogd/common/models/enums/ExtractionMode.py
+++ src/ogd/common/models/enums/ExtractionMode.py
@@ -3,7 +3,11 @@ from enum import IntEnum
 
 class ExtractionMode(IntEnum):
     """Simple enum to represent the different levels of granularity at which extractions can be carried out:
-    Session, Player, Population, or Detector.
+    Session, Player, Population.
+
+    At some point in the future, goal will be to replace `AggregationMode` with `Aggregator`,
+    which will just define some sort of rule for dividing events up into smaller bunches.
+    These will probably just exist at the `ogd-core` level, and `AggregationMode` will be deprecated and removed from `ogd-common`.
     """
     SESSION = 1
     PLAYER = 2
